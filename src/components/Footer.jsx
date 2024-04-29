e692d5e6b82917e4af187da103955758940c71c9
ankit mangal <ankit_mangal@epam.com>
2024-04-29 14:20:48 +0530

    fixing styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
     <div className="flex justify-center my-7 items-center gap-2 p-30 bg-slate-600 rounded-md text-white max-[800px]:w-[100%] h-[44px] max-[800px]:px-3">
-        <p className="text-md select-none max-[800px]:text-[12px] max-[700px]:text-[10px]">Weather data provided by OpenWeather</p>
+        <p className="text-[14px] select-none max-[800px]:text-[12px] max-[700px]:text-[10px]">Weather data provided by OpenWeather</p>
       </div>
     </>
   )
